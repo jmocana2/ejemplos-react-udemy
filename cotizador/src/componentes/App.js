8e2cf7eba903ae544add121c445dcbec075fd98e
jose.ocana <telecomu4@gmail.com>
2018-11-20 09:27:24 +0100

    Organizando proyecto

---
+++ cotizador/src/componentes/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+     <div className="contenedor">Hola mundo</div>
+    );
+  }
+}
+
+export default App;
