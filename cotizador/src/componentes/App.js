2798c5a5d7868135c566186d97c3942bfcb30dd2
jose.ocana <telecomu4@gmail.com>
2018-11-22 10:04:20 +0100

    Created components Header and Formulario

--- cotizador/src/componentes/App.js
+++ cotizador/src/componentes/App.js
@@ -1,9 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Header from './Header';
+import Formulario  from './Formulario';
 
 class App extends Component {
   render() {
     return (
-     <div className="contenedor">Hola mundo</div>
+    <div className="contenedor">  
+      <div className="contenido">
+        <Header />
+        <Formulario />
+      </div>
+    </div>  
     );
   }
 }
