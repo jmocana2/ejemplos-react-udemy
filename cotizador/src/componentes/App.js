5e48a89f7f022baa9bf4d93acc8ae5d7034c4448
jose.ocana <telecomu4@gmail.com>
2018-12-03 09:40:36 +0100

    created method cotizarSeguro

--- cotizador/src/componentes/App.js
+++ cotizador/src/componentes/App.js
@@ -3,13 +3,18 @@ import Header from './Header';
 import Formulario  from './Formulario';
 
 class App extends Component {
+
+  cotizarSeguro = (datos) => {
+    console.log(datos);
+  }
+
   render() {
     return (
     <div className="contenedor">  
       <div className="contenido">
         <Header />
         <div className="contenedor-formulario">
-          <Formulario />
+          <Formulario cotizarSeguro={this.cotizarSeguro} />
         </div>
       </div>
     </div>  
