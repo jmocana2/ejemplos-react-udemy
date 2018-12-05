f0ffa9d0c6fa4a78134bb4446f52119dca962fc2
jmocana2 <telecomu4@gmail.com>
2018-12-05 09:11:17 +0100

    using helper functions

--- cotizador/src/componentes/App.js
+++ cotizador/src/componentes/App.js
@@ -1,11 +1,31 @@
 import React, { Component, Fragment } from 'react';
 import Header from './Header';
 import Formulario  from './Formulario';
+import { obtenerDiferenciaAnio, calcularMarca, obtenerPlan } from '../helper';
 
 class App extends Component {
 
   cotizarSeguro = (datos) => {
-    console.log(datos);
+    const {marca, year, plan} = datos;
+
+    //Agregar una base de 2000
+    let resultado = 2000;
+
+    //Obtener la diferencia de años
+    const diferencia = obtenerDiferenciaAnio(year);
+    
+    //Por cada año restar el 3% del valor del seguro
+    resultado -= ((diferencia * 3) * resultado) / 100
+    
+    //Americano 15%, Asiático 5% y Europeo 30% del valor
+    const incrementoMarca = calcularMarca(marca);
+    resultado = resultado * incrementoMarca;   
+
+    //Plan básico aumenta 15%, plan completo 50%
+    const incrementoPlan = obtenerPlan(plan);
+    resultado = parseFloat(resultado * incrementoPlan).toFixed(2);
+
+    console.log(resultado);
   }
 
   render() {
