17ea656d8c68650cd28a83789e97f20570e3e127
jose.ocana <telecomu4@gmail.com>
2018-12-19 10:16:53 +0100

    Created refs

--- gastosemanal/src/componentes/Formulario.js
+++ gastosemanal/src/componentes/Formulario.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
 
 class FormularioGasto extends Component {
+
+  nombreGasto = React.createRef();
+  cantidadGasto = React.createRef();
+
+  crearGasto = (e) => {
+    e.preventDefault();
+    
+    console.log(`nombre: ${this.nombreGasto.current.value}, cantidad: ${this.cantidadGasto.current.value}` )
+  }
+  
   render() {
-    return (
-      <form>
+     return (
+      <form onSubmit={this.crearGasto}>
         <h2>Agrega tus gastos aqui</h2>
         <div className="campo">
             <label>Nombre Gasto</label>
-            <input className="u-full-width" type="text" placeholder="Ej. Transporte" />
+            <input ref={this.nombreGasto} className="u-full-width" type="text" placeholder="Ej. Transporte" />
         </div>
 
         <div className="campo">
             <label>Cantidad</label>
-            <input className="u-full-width" type="text" placeholder="Ej. 300" />
+            <input ref={this.cantidadGasto} className="u-full-width" type="text" placeholder="Ej. 300" />
         </div>
 
         <input className="button-primary u-full-width" type="submit" value="Agregar" />
