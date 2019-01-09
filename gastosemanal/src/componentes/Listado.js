26bee7014e1f14c7969fc7947f778680b59423b3
jose.ocana <telecomu4@gmail.com>
2019-01-09 09:34:42 +0100

    showing expenses and applying styles

--- gastosemanal/src/componentes/Listado.js
+++ gastosemanal/src/componentes/Listado.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
-import Gasto from './Gato';
+import Gasto from './Gasto';
 
 class Listado extends Component {
   render() {
     return (
-      <div>
+      <div className="gastos-realizados">
         <h2>Listado</h2>
-        {Object.keys(this.props.gastos).map(key => (
-          <Gasto key={key} />
-        ))} 
-        {console.log(this.props.gastos)}
+        <ul>
+          {Object.keys(this.props.gastos).map(key => (
+            <Gasto key={key} gasto={this.props.gastos[key]} />
+          ))}       
+        </ul> 
       </div>
     );
   }
