00ebd8fd8ff9d9eea764006ba2baf2dad23416db
jose.ocana <telecomu4@gmail.com>
2019-01-16 09:21:16 +0100

    Created AgregarCita event onSumit and crearCita functions

--- citasreact/src/App.js
+++ citasreact/src/App.js
@@ -3,13 +3,18 @@ import Header from './componentes/Header';
 import AgregarCita from './componentes/AgregarCita';
 
 class App extends Component {
+
+  crearCita = () => {
+    console.log('desde app')
+  }
+
   render() {
     return (
       <div className="container">
         <Header />
         <div className="row">
           <div className="col-md-6">
-            <AgregarCita />
+            <AgregarCita crearCita={this.crearCita} />
           </div>
         </div>
         
--- citasreact/src/componentes/AgregarCita.js
+++ citasreact/src/componentes/AgregarCita.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 
 class AgregarCita extends Component {
+
+  crearNuevaCita = (e) => {
+    e.preventDefault();
+
+    this.props.crearCita()
+  }
+
   render() {
     return (
       <div>
         <div className="card mt-5">
           <div className="card-body">
             <h2 className="card-title text-center mb-5">Agregar las citas aquí</h2>
-            <form >
+            <form onSubmit={this.crearNuevaCita}>
               <div className="form-group row">
                   <label className="col-sm-4 col-lg-2 col-form-label">Nombre Mascota</label>
                   <div className="col-sm-8 col-lg-10">
