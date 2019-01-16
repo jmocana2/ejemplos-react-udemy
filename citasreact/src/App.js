2ffa2696fdbe494b215bbcafc1a6947ed8897da3
jose.ocana <telecomu4@gmail.com>
2019-01-16 09:00:39 +0100

    Created Header

--- citasreact/src/App.js
+++ citasreact/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import Header from './componentes/header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       Hola citas!!
+        <Header />
       </div>
     );
   }
