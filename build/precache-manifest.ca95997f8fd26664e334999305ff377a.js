cc0fa7cd1c8d2099d3afd984cafaec54e1a4958d
jmocana <telecomu4@gmail.com>
2019-02-05 19:08:43 +0100

    generated build for Netlify

---
+++ build/precache-manifest.ca95997f8fd26664e334999305ff377a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f817b4e2608676389f43",
+    "url": "/static/js/main.f817b4e2.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "f817b4e2608676389f43",
+    "url": "/static/css/main.fb11ee93.chunk.css"
+  },
+  {
+    "revision": "4d0799a50d8b34a930ac5cc2998aec8a",
+    "url": "/index.html"
+  }
+];
