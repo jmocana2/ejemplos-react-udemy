e3ece4905602446d4a908c352820034941fbcd07
jose.ocana <telecomu4@gmail.com>
2019-03-25 09:18:30 +0100

    Created component Producto

--- reduxcrud/src/componentes/Producto.js
+++ reduxcrud/src/componentes/Producto.js
@@ -1,15 +1,26 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Producto extends Component {
   render() {
 
-    const { nombre, precio } = this.props
+    const { nombre, precio, id } = this.props
 
     return (
-      <div>
-        <h1>{nombre}</h1>
-        <p>Este producto cuesta: {precio}&euro;</p>
-      </div>
+      <li className="list-group-item">
+        <div className="row justify-content-between align-items-center">
+          <div className="col-md-8 d-flex justify-content-between align-items-center">
+            <p className="text-dark m-0">
+              {nombre}
+            </p>
+            <span className="badge badge-warning text-dark">$ {precio}</span>
+          </div>
+          <div className="col-md-4 d-flex justify-content-end acciones">
+            <Link to={`productos/editar/${id}`} className="btn btn-primary mr-2">Editar</Link>
+            <button type="button" className="btn btn-danger">Borrar</button>
+          </div>
+        </div>
+      </li>
     );
   }
 }
